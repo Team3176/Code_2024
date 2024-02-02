36b6a8e8ff776734ecfba27b30d141ad5dbe8d5e
Malcolm Laptop <char3176.malcolm@gmail.com>
2024-02-01 20:56:50 -0500

    Started to Add velocityvoltage to Shooter
    not done yet so dont use without checking

--- src/main/java/team3176/robot/subsystems/superstructure/Shooter.java
+++ src/main/java/team3176/robot/subsystems/superstructure/Shooter.java
@@ -20,16 +20,22 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team3176.robot.constants.SuperStructureConstants;
+import team3176.robot.subsystems.superstructure.Shooter.States;
 import team3176.robot.Constants;
 import team3176.robot.Constants.Mode;
+import com.ctre.phoenix6.controls.NeutralOut;
 import org.littletonrobotics.junction.Logger;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Shooter extends SubsystemBase {
     private static Shooter instance;
     private final ShooterIO io;
     private final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
-        
+    private final VelocityVoltage m_voltageVelocity = new VelocityVoltage(0, 0, true, 0, 0, false, false, false);  
+    private final NeutralOut m_brake = new NeutralOut();  
     private final PIDController turningPIDController;
+    private final TalonFX m_fx = new TalonFX(0);
     public enum States {OPEN_LOOP,CLOSED_LOOP}
     private States currentState = States.OPEN_LOOP;
     private double armSetpointAngleRaw = SuperStructureConstants.SHOOTER_ZERO_POS;
@@ -170,7 +176,19 @@ public class Shooter extends SubsystemBase {
         return this.runEnd(this::armAnalogDown, this::idle);
     }
     
-    
+    @Override
+    public void robotInit() {
+      TalonFXConfiguration configs = new TalonFXConfiguration();
+  
+      /* Voltage-based velocity requires a feed forward to account for the back-emf of the motor */
+      configs.Slot0.kP = 0.11; // An error of 1 rotation per second results in 2V output
+      configs.Slot0.kI = 0.5; // An error of 1 rotation per second increases output by 0.5V every second
+      configs.Slot0.kD = 0.0001; // A change of 1 rotation per second squared results in 0.01 volts output
+      configs.Slot0.kV = 0.12; // Falcon 500 is a 500kV motor, 500rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / Rotation per second
+      // Peak output of 8 volts
+      configs.Voltage.PeakForwardVoltage = 8;
+      configs.Voltage.PeakReverseVoltage = -8;
+    }
     @Override
     public void periodic() {
         io.updateInputs(inputs);
@@ -199,4 +217,26 @@ public class Shooter extends SubsystemBase {
             setPIDPosition(armSetpointAngleRaw);
         }
     }
+    @Override
+    public void robotPeriodic() {
+        m_mechanisms.update(m_fx.getPosition(), m_fx.getVelocity());
+    }
+    @Override
+    public void ShooterPIDVelocity(double Joyvalue) {
+        if (joyValue > -0.1 && joyValue < 0.1) joyValue = 0;
+
+        double desiredRotationsPerSecond = joyValue * 50; // Go for plus/minus 10 rotations per second
+        if (Math.abs(desiredRotationsPerSecond) <= 1) { // Joystick deadzone
+        desiredRotationsPerSecond = 0;
+        }
+        if (m_joystick.getLeftBumper()) {
+        /* Use voltage velocity */
+        m_fx.setControl(m_voltageVelocity.withVelocity(desiredRotationsPerSecond));
+        return m_fx.getaver
+        }
+        else {
+        /* Disable the motor instead */
+        m_fx.setControl(m_brake);
+        
+  }
 }
