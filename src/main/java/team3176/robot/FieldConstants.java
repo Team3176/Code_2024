ea4229b0a2d924c24bcafdce3ada5409c18399a2
jheidegg <jheidegg@umich.edu>
2024-03-02 00:50:11 -0500

    updating fieldConstants

--- src/main/java/team3176/robot/FieldConstants.java
+++ src/main/java/team3176/robot/FieldConstants.java
@@ -1,33 +1,35 @@
 package team3176.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
 public class FieldConstants {
-  public static double fieldLength = Units.inchesToMeters(651.223);
-  public static double fieldWidth = Units.inchesToMeters(323.277);
-  public static double wingX = Units.inchesToMeters(229.201);
-  public static double podiumX = Units.inchesToMeters(126.75);
-  public static double startingLineX = Units.inchesToMeters(74.111);
+  public static final double fieldLength = Units.inchesToMeters(651.223);
+  public static final double fieldWidth = Units.inchesToMeters(323.277);
+  public static final double wingX = Units.inchesToMeters(229.201);
+  public static final double podiumX = Units.inchesToMeters(126.75);
+  public static final double startingLineX = Units.inchesToMeters(74.111);
 
-  public static Translation2d ampCenter =
-      new Translation2d(Units.inchesToMeters(72.455), Units.inchesToMeters(322.996));
+  public static final Translation2d ampCenter =
+      new Translation2d(Units.inchesToMeters(72.455), fieldWidth);
 
   /** Staging locations for each note */
   public static final class StagingLocations {
-    public static double centerlineX = fieldLength / 2.0;
+    public static final double centerlineX = fieldLength / 2.0;
 
     // need to update
-    public static double centerlineFirstY = Units.inchesToMeters(29.638);
-    public static double centerlineSeparationY = Units.inchesToMeters(66);
-    public static double spikeX = Units.inchesToMeters(114);
+    public static final double centerlineFirstY = Units.inchesToMeters(29.638);
+    public static final double centerlineSeparationY = Units.inchesToMeters(66);
+    public static final double spikeX = Units.inchesToMeters(114);
     // need
-    public static double spikeFirstY = Units.inchesToMeters(161.638);
-    public static double spikeSeparationY = Units.inchesToMeters(57);
+    public static final double spikeFirstY = Units.inchesToMeters(161.638);
+    public static final double spikeSeparationY = Units.inchesToMeters(57);
 
-    public static Translation2d[] centerlineTranslations = new Translation2d[5];
-    public static Translation2d[] spikeTranslations = new Translation2d[3];
+    public static final Translation2d[] centerlineTranslations = new Translation2d[5];
+    public static final Translation2d[] spikeTranslations = new Translation2d[3];
 
     static {
       for (int i = 0; i < centerlineTranslations.length; i++) {
@@ -47,30 +49,78 @@ public class FieldConstants {
   public static final class Speaker {
 
     // corners (blue alliance origin)
-    public static Translation3d topRightSpeaker =
+    public static final Translation3d topRightSpeaker =
         new Translation3d(
             Units.inchesToMeters(18.055),
             Units.inchesToMeters(238.815),
-            Units.inchesToMeters(13.091));
+            Units.inchesToMeters(83.091));
 
-    public static Translation3d topLeftSpeaker =
+    public static final Translation3d topLeftSpeaker =
         new Translation3d(
             Units.inchesToMeters(18.055),
             Units.inchesToMeters(197.765),
             Units.inchesToMeters(83.091));
 
-    public static Translation3d bottomRightSpeaker =
+    public static final Translation3d bottomRightSpeaker =
         new Translation3d(0.0, Units.inchesToMeters(238.815), Units.inchesToMeters(78.324));
-    public static Translation3d bottomLeftSpeaker =
+    public static final Translation3d bottomLeftSpeaker =
         new Translation3d(0.0, Units.inchesToMeters(197.765), Units.inchesToMeters(78.324));
 
     /** Center of the speaker opening (blue alliance) */
-    public static Translation3d centerSpeakerOpening =
-        new Translation3d(
-            topLeftSpeaker.getX() / 2.0,
-            fieldWidth - Units.inchesToMeters(104.0),
-            (bottomLeftSpeaker.getZ() + bottomRightSpeaker.getZ()) / 2.0);
+    public static final Translation3d centerSpeakerOpening =
+        bottomLeftSpeaker.interpolate(topRightSpeaker, 0.5);
   }
 
-  public static double aprilTagWidth = Units.inchesToMeters(6.50);
+  public static final class Subwoofer {
+    public static final Pose2d ampFaceCorner =
+        new Pose2d(
+            Units.inchesToMeters(35.775),
+            Units.inchesToMeters(239.366),
+            Rotation2d.fromDegrees(-120));
+
+    public static final Pose2d sourceFaceCorner =
+        new Pose2d(
+            Units.inchesToMeters(35.775),
+            Units.inchesToMeters(197.466),
+            Rotation2d.fromDegrees(120));
+
+    public static final Pose2d centerFace =
+        new Pose2d(
+            Units.inchesToMeters(35.775),
+            Units.inchesToMeters(218.416),
+            Rotation2d.fromDegrees(180));
+  }
+
+  public static final class Stage {
+    public static final Pose2d podiumLeg =
+        new Pose2d(Units.inchesToMeters(126.75), Units.inchesToMeters(161.638), new Rotation2d());
+    public static final Pose2d ampLeg =
+        new Pose2d(
+            Units.inchesToMeters(220.873),
+            Units.inchesToMeters(212.425),
+            Rotation2d.fromDegrees(-30));
+    public static final Pose2d sourceLeg =
+        new Pose2d(
+            Units.inchesToMeters(220.873),
+            Units.inchesToMeters(110.837),
+            Rotation2d.fromDegrees(30));
+
+    public static final Pose2d centerPodiumAmpChain =
+        new Pose2d(
+            podiumLeg.getTranslation().interpolate(ampLeg.getTranslation(), 0.5),
+            Rotation2d.fromDegrees(120.0));
+    public static final Pose2d centerAmpSourceChain =
+        new Pose2d(
+            ampLeg.getTranslation().interpolate(sourceLeg.getTranslation(), 0.5), new Rotation2d());
+    public static final Pose2d centerSourcePodiumChain =
+        new Pose2d(
+            sourceLeg.getTranslation().interpolate(podiumLeg.getTranslation(), 0.5),
+            Rotation2d.fromDegrees(240.0));
+    public static final Pose2d center =
+        new Pose2d(Units.inchesToMeters(192.55), Units.inchesToMeters(161.638), new Rotation2d());
+    public static final double centerToChainDistance =
+        center.getTranslation().getDistance(centerPodiumAmpChain.getTranslation());
+  }
+
+  public static final double aprilTagWidth = Units.inchesToMeters(6.50);
 }
