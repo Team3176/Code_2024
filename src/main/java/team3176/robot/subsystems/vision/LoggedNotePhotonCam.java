abe1b5d4a531bdd777d0904f827b0430eee5a605
jheidegger <jth3141@gmail.com>
2024-03-22 01:24:11 -0400

    visualizations for notecam

--- src/main/java/team3176/robot/subsystems/vision/LoggedNotePhotonCam.java
+++ src/main/java/team3176/robot/subsystems/vision/LoggedNotePhotonCam.java
@@ -1,7 +1,10 @@
 package team3176.robot.subsystems.vision;
 
+import java.util.ArrayList;
 import org.littletonrobotics.junction.Logger;
+import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.targeting.TargetCorner;
 
 public class LoggedNotePhotonCam {
 
@@ -17,10 +20,32 @@ public class LoggedNotePhotonCam {
     this.inputs = new PhotonCameraInputsAutoLogged();
   }
 
+  public void logExtra(PhotonPipelineResult results) {
+    ArrayList<Double> x = new ArrayList<Double>();
+    ArrayList<Double> y = new ArrayList<Double>();
+    if (results.hasTargets()) {
+
+      for (PhotonTrackedTarget t : results.getTargets()) {
+        for (TargetCorner c : t.getMinAreaRectCorners()) {
+          x.add(c.x);
+          y.add(c.y);
+        }
+      }
+
+    } else {
+
+    }
+    double[] xarray = x.stream().mapToDouble(Double::doubleValue).toArray();
+    double[] yarray = y.stream().mapToDouble(Double::doubleValue).toArray();
+    Logger.recordOutput("photonvision/" + name + "/xarray", xarray);
+    Logger.recordOutput("photonvision/" + name + "/yarray", yarray);
+  }
+
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("photonvision/" + this.name, inputs);
     var results = inputs.results;
+    logExtra(results);
     if (results.hasTargets()) {
       PhotonTrackedTarget target = results.getBestTarget();
       Logger.recordOutput("photonvision/noteyaw", target.getYaw());
