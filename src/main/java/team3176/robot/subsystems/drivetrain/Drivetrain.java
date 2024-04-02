27a0132cf260c3f3c51918af948bd59808b844a9
char@odin <char3176@gmail.com>
2024-04-02 04:35:54 -0400

    updating to fxnal code

--- src/main/java/team3176/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/team3176/robot/subsystems/drivetrain/Drivetrain.java
@@ -342,9 +342,10 @@ public class Drivetrain extends SubsystemBase {
     Pose2d currentPose = getPose();
     double futureX = currentPose.getX() * speeds.vxMetersPerSecond * offsetSec;
     double futureY = currentPose.getY() * speeds.vyMetersPerSecond * offsetSec;
-    double futureRotation2d  = currentPose.getRotation().getRadians() * speeds.omegaRadiansPerSecond * offsetSec;
-    Pose2d futurePose; 
-    futurePose = Pose2d(futureX, futureY, Rotation2d.fromRadians(futureRotation2d)futureRotation2d));
+    double futureRotation2d =
+        currentPose.getRotation().getRadians() * speeds.omegaRadiansPerSecond * offsetSec;
+    Pose2d futurePose;
+    futurePose = new Pose2d(futureX, futureY, Rotation2d.fromRadians(futureRotation2d));
     return odom.getPoseMeters();
   }
 
--- src/main/java/team3176/robot/subsystems/superstructure/shooter/Shooter.java
+++ src/main/java/team3176/robot/subsystems/superstructure/shooter/Shooter.java
@@ -8,9 +8,9 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.Timer;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 import team3176.robot.Constants;
@@ -260,7 +260,10 @@ public class Shooter extends SubsystemBase {
   @Override
   public void periodic() {
     io.updateInputs(inputs);
-    this.futureDistance = getDistance() * ((getDistance() - this.lastDistance) / this.lastDistance) * (Timer.getFPGATimestamp() - this.lastTimestamp);
+    this.futureDistance =
+        getDistance()
+            * ((getDistance() - this.lastDistance) / this.lastDistance)
+            * (Timer.getFPGATimestamp() - this.lastTimestamp);
     this.lastDistance = getDistance();
     this.lastTimestamp = Timer.getFPGATimestamp();
     pivotPIDController.checkParemeterUpdate();
