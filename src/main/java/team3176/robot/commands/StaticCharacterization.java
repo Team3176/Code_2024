e48c3b9c5f3c98b0029688a7925d18c89f6de222
jheidegger <jth3141@gmail.com>
2024-04-29 16:00:57 -0400

    adding advantage ID routines

---
+++ src/main/java/team3176/robot/commands/StaticCharacterization.java
@@ -0,0 +1,59 @@
+// Copyright (c) 2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package team3176.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import team3176.robot.util.LoggedTunableNumber;
+
+import java.util.function.DoubleConsumer;
+import java.util.function.DoubleSupplier;
+
+public class StaticCharacterization extends Command {
+  private static final LoggedTunableNumber currentRampFactor =
+      new LoggedTunableNumber("StaticCharacterization/CurrentRampPerSec", 1.0);
+  private static final LoggedTunableNumber minVelocity =
+      new LoggedTunableNumber("StaticCharacterization/MinStaticVelocity", 0.1);
+
+  private final DoubleConsumer inputConsumer;
+  private final DoubleSupplier velocitySupplier;
+  private final Timer timer = new Timer();
+  private double currentInput = 0.0;
+
+  public StaticCharacterization(
+      Subsystem subsystem,
+      DoubleConsumer characterizationInputConsumer,
+      DoubleSupplier velocitySupplier) {
+    inputConsumer = characterizationInputConsumer;
+    this.velocitySupplier = velocitySupplier;
+    addRequirements(subsystem);
+  }
+
+  @Override
+  public void initialize() {
+    timer.restart();
+  }
+
+  @Override
+  public void execute() {
+    currentInput = timer.get() * currentRampFactor.get();
+    inputConsumer.accept(currentInput);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return velocitySupplier.getAsDouble() >= minVelocity.get();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    System.out.println("Static Characterization output: " + currentInput + " amps");
+    inputConsumer.accept(0);
+  }
+}
