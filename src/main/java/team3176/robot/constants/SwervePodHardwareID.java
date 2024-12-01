dcddd65f5d47b5fe1318920b3057caa1c434221c
char@odin <char3176@gmail.com>
2024-11-30 20:58:17 -0500

    Beginning MKPod conversion

--- src/main/java/team3176/robot/constants/SwervePodHardwareID.java
+++ src/main/java/team3176/robot/constants/SwervePodHardwareID.java
@@ -7,6 +7,8 @@ public class SwervePodHardwareID {
   public int SERIAL;
   public int THRUST_CID;
   public String THRUST_CBN;
+  public int AZIMUTH_CID;
+  public String AZIMUTH_CBN;
   public int CANCODER_CID;
   public String CANCODER_CBN;
   public double OFFSET;
@@ -14,9 +16,11 @@ public class SwervePodHardwareID {
   SwervePodHardwareID() {}
 
   SwervePodHardwareID(
-      int thrust_cid, String thrust_cbn, int cancoder_cid, String cancoder_cbn, double offset) {
+      int thrust_cid, String thrust_cbn, int azimuth_cid, String azimuth_cbn, int cancoder_cid, String cancoder_cbn, double offset) {
     this.THRUST_CID = thrust_cid;
     this.THRUST_CBN = thrust_cbn;
+    this.AZIMUTH_CID = azimuth_cid;
+    this.AZIMUTH_CBN = azimuth_cbn;
     this.CANCODER_CID = cancoder_cid;
     this.CANCODER_CBN = cancoder_cbn;
     this.OFFSET = offset;
@@ -26,11 +30,15 @@ public class SwervePodHardwareID {
       int serial,
       int thrust_cid,
       String thrust_cbn,
+      int azimuth_cid,
+      String azimuth_cbn,
       int cancoder_cid,
       String cancoder_cbn,
       double offset) {
     this.THRUST_CID = thrust_cid;
     this.THRUST_CBN = thrust_cbn;
+    this.AZIMUTH_CID = azimuth_cid;
+    this.AZIMUTH_CBN = azimuth_cbn;
     this.CANCODER_CID = cancoder_cid;
     this.CANCODER_CBN = cancoder_cbn;
     this.SERIAL = serial;
